2232a405c78dadd6a20d1a6627ceaa63db46c65a
Jefferson Correia <correia_jefferson@outlook.com>
2025-03-30 18:15:58 -0300

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Erro interno do servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404);
+        res.end('Página não encontrada');
+    }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
